9a10cc3bdbf810ef7d25efca02b2bd911a197a13
AlvaroVidal <alvaro.vidal1@unmsm.edu.pe>
2023-08-23 17:46:54 -0500

    Ejercicio practico 002

--- Dalto/1_Seccion_Intermedia/Codigo/006_ejerciciosPracticos_02.py
+++ Dalto/1_Seccion_Intermedia/Codigo/006_ejerciciosPracticos_02.py
@@ -1,38 +1,40 @@
+# 1 profesor -> el que más edad tiene
+# 1 asistente -> el que menos edad tiene
+
+# Ingresar los nombres y edades de los alumnos (dentro de ellos está el profesor y el asistente) y 
+# ordenar de mayor a menor
 
 
 
-dict_alumnos = dict()
+alumnos = list()
 
 
-def ingresar_alumno():
-    contador = 0
-    numero_alumnos = int(input("¿Cuántos alumnos hay en salón? "))
-    while contador < numero_alumnos:
-        alumno = input("Cuál es tu nombre: ")
-        edad = int(input("Cuál es tu edad: "))
-        dict_alumnos[alumno] = edad
-        contador += 1
+def obtener_alumnos(cantidad_de_alumnos):
+    for alumno in range(cantidad_de_alumnos):
+        nombre_alumno = input('Cuál es el nombre del alumno: ')
+        edad_alumno = int(input('Cuál es la edad del alumno: '))
+        alumno_input = (nombre_alumno, edad_alumno)
+        alumnos.append(alumno_input)
 
-    
-test = {
-    "key1" : 5,
-    "key2" : 4,
-    "key3" : 3
-}
+    alumnos.sort(key= lambda x:x[1]) # x[1] x es la tupla ("alumno", edad), entonces x[1] es la edad, usando lambda pedimos que nos ordenen por la edad. El sort va a ordenar de MENOR -> MAYOR
 
+    # El asistente el el el primero en la lista ordenada
+    asistente = alumnos[0][0] # Esto para obtener solo el nombre del asistente
 
+    # El profesor el el último de la lista, dado que es el que tiene mayor edad
+    profesor = alumnos[-1][0] 
 
-
+    return asistente, profesor
+ 
 
 def main():
-    print(list(test.))
+    asistente, profesor = obtener_alumnos(3)
+    print(f'las personas en la clase son {alumnos}')
+    print(f'El asistente es: {asistente}')
+    print(f'El profesor es: {profesor}')
+  
 
 
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
